51681566e0202565369c6da5f9c671fcaaae2170
amy327327 <327amyamy@gmail.com>
2025-06-26 12:31:04 +0800

    add ds_04.java

---
+++ 0626/ds_04.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class ds_04 {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        int[] numbers = {10, 20, 30, 40, 50};
+        System.out.println("陣列中的元素如下：");
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println("第 " + (i + 1) + " 個元素是：" + numbers[i]);
+        }
+
+    
+}}
